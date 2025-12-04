c0fdf088f6cc233d61b06fa6e1472aaef7b6a27d
= <=>
2025-12-04 15:00:55 +0100

    feature(app): day-2 Day 2 resolved

---
+++ Day 2/IdValidator.cs
@@ -0,0 +1,40 @@
+﻿namespace Day_2
+{
+	internal static class IdValidator
+	{
+		public static long ValidateRanges(IEnumerable<string> ranges)
+		{
+			long total = 0;
+			foreach (var range in ranges)
+			{
+				var splittedRange = range.Split('-');
+				var min = long.Parse(splittedRange[0]);
+				var max = long.Parse(splittedRange[1]);
+
+				var counter = min;
+
+				while (counter <= max)
+				{
+					var counterStr = counter.ToString();
+					if (counterStr.Length % 2 != 0)
+					{
+						counter++;
+						continue;
+					}
+
+					var counterStrLeft = counterStr.Substring(0, counterStr.Length / 2);
+					var counterStrRight = counterStr.Substring((counterStr.Length / 2));
+
+					if (counterStrLeft == counterStrRight)
+					{
+						total += counter;
+					}
+
+					counter++;
+				}
+			}
+
+			return total;
+		}
+	}
+}
---
+++ Day 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Day_2;
+
+var ranges = RangeReader.ReadRanges();
+
+var result = IdValidator.ValidateRanges(ranges);
+
+Console.WriteLine($"Result is: {result}");
+
+Console.ReadKey();
